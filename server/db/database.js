898c06bee35a95ea0b0cff96c932ea35b0718799
Claire Gilligan <feminaprovita@gmail.com>
2019-07-09 18:40:41 -0400

    added a process.env option

--- server/db/database.js
+++ server/db/database.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const chalk = require('chalk');
-const Sequelize = require('sequelize');
-const pkg = require('../../package.json');
+const chalk = require('chalk')
+const Sequelize = require('sequelize')
+const pkg = require('../../package.json')
 
-console.log(chalk.yellow('Opening database connection'));
+console.log(chalk.yellow('Opening database connection'))
 
-const db = new Sequelize(`postgres://localhost:5432/${pkg.name}`, {
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${pkg.name}`, {
   logging: false,
-});
+})
 
 module.exports = db;
