fcb77f914fb978d5581f1f29b64222ac4edf0819
Claire Gilligan <feminaprovita@gmail.com>
2019-07-09 16:00:37 -0400

    updated packages per github security alerts, tweaked stretch goals

--- client/components/UnderConstruction.js
+++ client/components/UnderConstruction.js
@@ -11,8 +11,19 @@ const Construction = () => {
         <li>View Questions by Tag</li>
         <li>View 5 Random Questions</li>
         <li>Suggestion Box</li>
-        <li>Twitter Bot (tweeting used questions sans link)</li>
-        <li>Display today's question on homepage (update automatically)</li>
+        <li>Display today's question on homepage</li>
+      </ul>
+      <br />
+      <br />
+      <h2>Behind-the-Scenes Features Under Development</h2>
+      <ul>
+        <li>Implementing Facebook's API</li>
+        <li>Writing a script to update database daily</li>
+      </ul>
+      <h2>Stretch Goals</h2>
+      <ul>
+        <li>Search/Filter by Number of Comments</li>
+        <li>Twitter Bot (tweeting text of used questions)</li>
       </ul>
     </div>
   )
